77ccd30ddd91940097ee2ec65b687d3fa5765e60
David H Hagan <david@davidhhagan.com>
2017-06-16 11:29:42 -0400

    unittests

--- smps/utils.py
+++ smps/utils.py
@@ -2,16 +2,26 @@
 # -*- coding: utf-8 -*-
 """
 """
+import requests
 
 def _get_bin_count(fpath, encoding='ISO-8859-1'):
     """Get the number of bins in the file."""
     bins = 0
-    with open(fpath, 'r', encoding=encoding) as f:
-        for line in f:
+    if 'http' in fpath:
+        req = requests.get(fpath)
+
+        for line in req.iter_lines():
             try:
-                if float(line.split(',')[0]):
+                if float(line.decode('utf-8').split(',')[0]):
                     bins += 1
             except: pass
+    else:
+        with open(fpath, 'r', encoding=encoding) as f:
+            for line in f:
+                try:
+                    if float(line.split(',')[0]):
+                        bins += 1
+                except: pass
 
     return bins
 
