970e642e2a166bf8cd4fb72308c68f00d6e1deda
David H. Hagan <david.hagan@quant-aq.com>
2023-07-15 07:41:40 -0400

    Updated the tests ci

--- smps/__init__.py
+++ smps/__init__.py
@@ -1,8 +1,8 @@
-from pkg_resources import get_distribution
+from importlib.metadata import version
 
 from .io import *
 from .plots import *
 from .models import *
 from .rcmod import *
 
-__version__ = get_distribution("py-smps").version
+__version__ = version("py-smps")
